cff792ce572fae1154cf7fae9a497bc227c78a46
arlo <arloalb@bu.edu>
2023-10-04 21:48:00 -0400

    add 4d and 5ab

--- albelli.arlo.hw4.lean
+++ albelli.arlo.hw4.lean
@@ -42,15 +42,85 @@ example {n : ℤ} (hn : Even n) : Odd (n ^ 2 + 2 * n - 5) := by
 
 /-4d-/
 example (a b c : ℤ) : Even (a - b) ∨ Even (a + c) ∨ Even (b - c) := by
-  dsimp [Even] at * 
+  obtain ha | ha := Int.even_or_odd a
+  · obtain hb | hb := Int.even_or_odd b
+    · left
+      obtain ⟨r, hr⟩ := ha
+      obtain ⟨s, hs⟩ := hb
+      use r - s
+      calc
+        a - b = 2 * r - b := by rw [hr]
+        _ = 2 * r - 2 * s := by rw [hs]
+        _ = (r - s) + (r - s) := by ring
+    · obtain hc | hc := Int.even_or_odd c
+      · right; left
+        obtain ⟨r, hr⟩ := ha
+        obtain ⟨s, hs⟩ := hc
+        use r + s
+        calc
+          a + c = 2 * r + c := by rw [hr]
+          _ = 2 * r + 2 * s := by rw [hs]
+          _ = (r + s) + (r + s) := by ring
+      · right; right
+        obtain ⟨r, hr⟩ := hb
+        obtain ⟨s, hs⟩ := hc
+        use r - s
+        calc
+          b - c = 2 * r + 1 - c := by rw [hr]
+          _ = 2 * r + 1 - (2 * s + 1) := by rw [hs]
+          _ = (r - s) + (r - s) := by ring
+  · obtain hb | hb := Int.even_or_odd b
+    · obtain hc | hc := Int.even_or_odd c
+      · right; right
+        obtain ⟨r, hr⟩ := hb
+        obtain ⟨s, hs⟩ := hc
+        use r - s
+        calc
+          b - c = 2 * r - c := by rw [hr]
+          _ = 2 * r - (2 * s) := by rw [hs]
+          _ = (r - s) + (r - s) := by ring
+      · right; left
+        obtain ⟨r, hr⟩ := ha
+        obtain ⟨s, hs⟩ := hc
+        use r + s + 1
+        calc
+          a + c = 2 * r + 1 + c := by rw [hr]
+          _ = 2 * r + 1 + (2 * s + 1) := by rw [hs]
+          _ = (r + s + 1) + (r + s + 1) := by ring
+    · left
+      obtain ⟨r, hr⟩ := ha
+      obtain ⟨s, hs⟩ := hb
+      use r - s
+      calc
+        a - b = 2 * r + 1 - b := by rw [hr]
+        _ = 2 * r + 1 - (2 * s + 1) := by rw [hs]
+        _ = (r - s) + (r - s) := by ring
 
 /-5a-/
 example {a b : ℝ} (h : ∀ x, x ≥ a ∨ x ≤ b) : a ≤ b := by
-  sorry
+  have h' : (a + b) / 2 ≥ a ∨ (a + b) / 2 ≤ b := by apply h
+  obtain ha | hb := h'
+  · calc
+    b = 2 * ((a + b) / 2) - a := by ring
+    _ ≥ 2 * a - a := by rel [ha]
+    _ = a := by ring
+  · calc
+    a = 2 * ((a + b) / 2) - b := by ring
+    _ ≤ 2 * b - b := by rel [hb]
+    _ = b := by ring
 
 /-5b-/
 example : ∃ c : ℝ, ∀ x y, x ^ 2 + y ^ 2 ≤ 4 → x + y ≥ c := by
-  sorry
+  use -3
+  intro x y
+  intro h
+  have h1 := calc
+    (x+y)^2 ≤ (x+y)^2 + (x-y)^2 := by extra
+    _ = 2 * (x^2 + y^2) := by ring
+    _ ≤ 2 * 4 := by rel [h]
+    _ ≤ (3)^2 := by numbers
+  have h2 : (0:ℝ) ≤ 3 := by numbers
+  exact And.left (abs_le_of_sq_le_sq' h1 h2)
 
 /-5c-/
 example {n : ℤ} (hn : ∀ m, 1 ≤ m → m ≤ 5 → m ∣ n) : 15 ∣ n := by
