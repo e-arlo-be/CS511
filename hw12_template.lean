facf221fc52bac1372cd3db2d0a21cf0c378bd96
arlo <arloalb@bu.edu>
2023-12-04 18:21:46 -0500

    done problem 4

--- hw12_template.lean
+++ hw12_template.lean
@@ -41,16 +41,77 @@ theorem problem4b {f : X → Y} (hf : Surjective f) : ∃ g : Y → X, f ∘ g =
   ext x
   rel [hg, x]
 
-/-
+
 /- 2 points -/
 theorem problem4c {f : X → Y} {g : Y → X} (h : Inverse f g) : Inverse g f := by
-  sorry
+  dsimp [Inverse] at *
+  obtain ⟨h1, h2⟩ := h
+  use h2
+  apply h1
 
 /- 3 points -/
 theorem problem4d {f : X → Y} {g1 g2 : Y → X} (h1 : Inverse f g1) (h2 : Inverse f g2) :
     g1 = g2 := by
-  sorry
+  dsimp [Inverse] at *
+  obtain ⟨hg1f, hfg1⟩ := h1
+  obtain ⟨hg2f, hfg2⟩ := h2
+  dsimp [id] at *
+  funext x
+  have heq : (f ∘ g1) x = (f ∘ g2) x := by rw [hfg1, hfg2]
+  have hg1x : (f ∘ g1) x = x := by rw [hfg1]
+  have hg2x : (f ∘ g2) x = x := by rw [hfg2]
+  have heq' : g1 ((f ∘ g1) x) = g1 ((f ∘ g2) x) := by rw [heq]
 
+  have hidk : g1 ((f ∘ g1) x) = (g1 x) := by rw [hg1x]
+  have hwe : g1 x = g2 x := by
+    calc
+      g1 x = g1 ((f ∘ g1) x) := by rw [hidk]
+      _ = g1 ((f ∘ g2) x) := by rw [heq']
+      _ = (g1 ∘ f ∘ g2) x := by rfl
+      _ = (g1 ∘ f) (g2 x) := by rfl
+      _ = g2 x := by rw [hg1f]
+  rw [hwe]
+  
+
+
+
+
+
+
+/-
+      _ = g2 ((f ∘ g2) ((f ∘ g2) x)) := by rw [←hg1f, ←hg2f]
+      _ = g2 ((f ∘ g2) ((f ∘ g2) x)) := by rw [←hg1x, ←hg2x, ←heq]
+      _ = g2 x := by rw [←hg2x]
+-/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/-
 /- 1 points -/
 theorem problem5a1 : ¬ Injective (fun ((x, y) : ℤ × ℤ) ↦ x - 2 * y - 1) := by
   sorry
